b7d9859814bfd847f319a4e6d894a0747843f17a
Oladipo Abiodun <oladipupoabiodun4@gmail.com>
2023-02-03 15:04:45 +0100

    Initial commit

---
+++ UserManagement System Demo/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement_System_Demo.Models
+{
+    public class Order : Base
+    {
+
+        public int? UserId { get; set; }
+
+        [Required]
+        
+        public int? Quantity { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public double Amount { get; set; }
+
+        public bool? IsCompleted { get; set; }
+
+        public bool? IsCancelled { get; set; }
+    }
+}
---
+++ UserManagement System Demo/Models/Support.cs
@@ -0,0 +1,26 @@
+﻿using ComponentModel.DataAnnotations;
+
+namespace UserManagement_System_Demo.Models
+{
+	public class support : Base
+	{
+		public string subject { get; set; }
+
+        [Required]
+        [StringLenght(MinimumLenght = 5, MaximumLenght = 30,
+        ErrorMessage = "{0} cannot be less than 5 and {1} cannot be greater than 30")]
+        public string Content { get; set; }
+
+        [Required]
+        public bool? Istreated { get; set; }
+
+        [Required]
+        public string status { get; set; }
+
+        [Required]
+        [Display(Name = "Sent By")]
+        public int SentById { get; set; }
+
+        
+    }
+}
